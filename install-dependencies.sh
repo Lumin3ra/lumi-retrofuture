c5493bfe2e2d8c92fefa04a42232aa63b3449881
Lumin3ra <llbluefire09@gmail.com>
2025-10-25 11:43:22 +0200

    started writing the install dependencies program

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,6 @@
-echo WIP
+#install stuff
+sudo pacman -S hyprpaper kitty waybar wob wofi yazi hyprpolkitagent nemo nautilus vscode fastfetch pavucontrol hyprshot
+#install yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+#install AUR stuff
+yay -S eww hyprwat
